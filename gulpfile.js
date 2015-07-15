277621061e9602c2cd148b0c5033868fe99b1fd0
Sota Yamashtia <sotayamashita@users.noreply.github.com>
2015-07-15 17:44:01 +0900

    Fix requirement order

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ var size    = require('gulp-size')
 gulp.task('style', function(){
   return gulp.src('app/styles/main.scss')
     .pipe( postcss([
-      require('postcss-nested'),
       require('postcss-mixins'),
       require('postcss-simple-vars'),
+      require('postcss-nested'),
       require('postcss-import'),
       require('postcss-will-change'),
       require('autoprefixer')({ browsers: ['last 1 versions'] })
