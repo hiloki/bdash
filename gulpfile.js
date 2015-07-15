9360ea86a614a85e95a992f86fa0af251be09a52
Sota Yamashita <sota.yamashita@gmail.com>
2015-07-15 19:57:04 +0900

    Fix: gulpfiles.js and package.json: js syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp    = require('gulp');
 var size    = require('gulp-size');
+var postcss = require('gulp-postcss');
 
 gulp.task('style', function() {
-  var postcss = require('gulp-postcss');
   return gulp.src('./postcss/main.css')
     .pipe( postcss([
       require('postcss-simple-vars'),
@@ -13,5 +13,14 @@ gulp.task('style', function() {
       require('autoprefixer')({ browsers: ['last 1 versions'] })
     ]) )
     .pipe( size({title: 'style'}) )
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('minify', function() {
+  var rename = require("gulp-rename");
+  return gulp.src('./dist/main.css')
+    .pipe( postcss([ require('cssnano')() ]))
+    .pipe( rename({ extname: '.min.css'}))
+    .pipe( size({title: 'minify'}) )
     .pipe(gulp.dest('./dist'))
 })
