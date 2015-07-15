319b6dd4fec21f3cb7341bdd36e9a90ecad55123
Sota Yamashtia <sotayamashita@users.noreply.github.com>
2015-07-15 17:44:50 +0900

    Fix requirement order simple-vars -> mixins

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var size    = require('gulp-size')
 gulp.task('style', function(){
   return gulp.src('app/styles/main.scss')
     .pipe( postcss([
-      require('postcss-mixins'),
       require('postcss-simple-vars'),
+      require('postcss-mixins'),
       require('postcss-nested'),
       require('postcss-import'),
       require('postcss-will-change'),
