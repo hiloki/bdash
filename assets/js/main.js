5e80948123230c387adae71598113fe8388126ae
Sota Yamashita <sota.yamashita@gmail.com>
2015-08-09 02:34:53 +0900

    Update docs js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,27 +4,29 @@
  */
 !(function () {
 
-  var message =  {
-    join: {
-      text: 'Are you interesed in this code ? => github()',
-      style: 'font-weight: bold; color: #44525a; font-size: 14px;'
-    },
-    code: {
-      text: 'Are you interesed in us ? =>  join()',
-      style: 'font-weight: bold; color: #44525a; font-size: 14px; background-image: url("http://www.emoji-cheat-sheet.com/graphics/emojis/octocat.png"); background-size: cover'
+  var styles = {
+    common: "font-weight: bold; color: #44525a; font-size: 14px;",
+    emoji: function(emoji) {
+      return "background-image: url(\"https://isitchristmas.com/emojis/" + emoji + ".png\"); background-size: cover"
     }
   }
 
-  console.log('%c' + message.code.text + ' %c %c', message.code.style)
-  console.log('%c ' + message.join.text, message.join.style)
-}());
+  var messages = {
+    code: "Are you interesed in code ? --> github()",
+    join: "Are you interesed in us ? -->  join()"
+  }
+
+  console.log("%c" + messages.code + "%c  ", styles.common, styles.emoji("octocat"))
+  console.log("%c" + messages.join + "%c  ", styles.common, styles.emoji("door"))
+
+}())
 
 
 /**
  * Redirect recruitment page
  */
 function join() {
-  var message = 'Open the http://sirok.co.jp/recruit/ in new tab in 3 seconds'
+  var message = 'Open the http://sirok.co.jp/recruit/ in 3 seconds'
   console.log('%c ' + message, 'color: #3E6DA7; background: #E4EDF4;')
   setTimeout('openRecuitmentPage()', 3000)
 }
@@ -34,5 +36,24 @@ function join() {
  */
 function openRecuitmentPage () {
   var recruitment_page = 'http://sirok.co.jp/recruit/'
-  window.open(recruitment_page)
+  window.location.href=recruitment_page
+}
+
+
+/**
+ * Redirect github page
+ */
+function github() {
+  var message = 'Open the https://github.com/sotayamashita/bdash in 3 seconds'
+  console.log('%c ' + message, 'color: #3E6DA7; background: #E4EDF4;')
+  setTimeout('openGithubPage()', 3000)
+}
+
+
+/**
+ * Open page
+ */
+function openGithubPage () {
+  var github_page = 'https://github.com/sotayamashita/bdash'
+  window.location.href=github_page
 }
